5dcab1b9729a53b3be81b1bee3fe594e9ce2b157
Ayanali786 <ayan.ahmed25508@gmail.com>
2022-01-03 03:00:59 -0800

    Updates

---
+++ precache-manifest.aaa4acb4e511b44632d6e683860ca3cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953cee748673cfadce29da899334478d",
+    "url": "/hosting-repo/index.html"
+  },
+  {
+    "revision": "8768b1627a409519c253",
+    "url": "/hosting-repo/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "31fb02b72edbfcb3346f",
+    "url": "/hosting-repo/static/css/main.6ab6137d.chunk.css"
+  },
+  {
+    "revision": "8768b1627a409519c253",
+    "url": "/hosting-repo/static/js/2.39349a19.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/hosting-repo/static/js/2.39349a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31fb02b72edbfcb3346f",
+    "url": "/hosting-repo/static/js/main.5901521c.chunk.js"
+  },
+  {
+    "revision": "5d16c1b91dbb6797c8a2",
+    "url": "/hosting-repo/static/js/runtime-main.82001dfc.js"
+  }
+]);
